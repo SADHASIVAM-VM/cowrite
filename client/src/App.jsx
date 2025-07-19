2a100b29357d16cb610d2fc667f57e40e52a9898
SADHASIVAM-VM <sadha4747@gmail.com>
2025-07-19 09:29:14 +0530

    updated...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,64 @@
-import {  BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import BlogListing from './pages/BlogListing'
-import PostArea from './pages/PostArea'
-import { ThemeProvider } from './components/Themeproviders'
-import SingleBlog from './pages/SingleBlog'
-import './App.css'
-import { Toaster } from 'react-hot-toast'
-import Save from './pages/Save'
-import NotFound from './pages/Notfound'
-import PrivateRoute from './Route/privateRoute'
-import { RedirectToSignIn  } from '@clerk/clerk-react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import BlogListing from "./pages/BlogListing";
+import PostArea from "./pages/PostArea";
+import { ThemeProvider } from "./components/Themeproviders";
+import SingleBlog from "./pages/SingleBlog";
+import "./App.css";
+import { Toaster } from "react-hot-toast";
+import Save from "./pages/Save";
+import NotFound from "./pages/Notfound";
+import PrivateRoute from "./Route/privateRoute";
+import { RedirectToSignIn } from "@clerk/clerk-react";
 
 const App = () => {
-// const {theme} = useMyContext();
+  // const {theme} = useMyContext();
   return (
     // <ThemeProvider defaultTheme='light'>
-    //    </ThemeProvider>  
+    //    </ThemeProvider>
 
     <>
-    
-  
-      <Toaster/>
-     
-    <BrowserRouter>
-    <Routes>
-      {/* public routes */}
-          <Route path='/' element={<Home/>} />
-          <Route path='/signin' element={<RedirectToSignIn />} />
-          <Route path='/blogs' element={<BlogListing/>}/>
+      <Toaster />
+
+      <BrowserRouter>
+        <Routes>
+          {/* public routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<RedirectToSignIn />} />
+          <Route path="/blogs" element={<BlogListing />} />
 
           {/* private routes */}
-          
-          
-          <Route path='/blog/:id' element={<PrivateRoute><SingleBlog/>  </PrivateRoute>}/>
-          <Route path='/post' element={<PrivateRoute><PostArea/></PrivateRoute>}/>
-          <Route path='/save' element={<PrivateRoute><Save/></PrivateRoute>}/>
-         
-          <Route path='*' element={<NotFound/>}/>
-          
 
+          <Route
+            path="/blog/:id"
+            element={
+              <PrivateRoute>
+                <SingleBlog />{" "}
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/post"
+            element={
+              <PrivateRoute>
+                <PostArea />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/save"
+            element={
+              <PrivateRoute>
+                <Save />
+              </PrivateRoute>
+            }
+          />
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
-    </BrowserRouter>
-   </>
-  )
-}
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
-
-
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,25 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ClerkProvider } from '@clerk/clerk-react'
-import { ContextApp } from './config/CommonContext.jsx'
-import { Router } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { ContextApp } from "./config/CommonContext.jsx";
 
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-if(!PUBLISHABLE_KEY){
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
   //console.log("MISSING PUBLISH KEY FROM @CLERK...")
 }
-createRoot(document.getElementById('root')).render(
-
-<>
-<ContextApp>
-<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-<App/>
-</ClerkProvider>
- </ContextApp>
-
+createRoot(document.getElementById("root")).render(
+  <>
+    <ContextApp>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
+    </ContextApp>
   </>
- 
-  
-  )
+);
